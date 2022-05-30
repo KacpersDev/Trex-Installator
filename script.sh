0335687612c074c3dc1ecfcb1705bedc3787075a
kacper <50886151+KacpersDev@users.noreply.github.com>
2022-05-30 18:28:44 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+echo "########################################"
+echo " Minecraft Server Installation Script"
+echo " 				Made by Trex           "
+echo "				Version: 0.1"
+echo "########################################"
+
+echo "Procced with the Installation? (y for yes, n for no)"
+read boolean
+if [ boolean = "y" ] then
+	echo "Installing [java, screen]"
+	apt-get install java
+	echo "Java has been installed. Installing screen"
+	apt-get install screen
+	
+	echo "Please enter a folder name you would like to have server(s) in"
+	read folder_name
+	cd /
+	mkdir $folder_name
+	cd $folder_name
+	echo "Folder has been created"
+	echo "Please enter a folder name for a server"
+	read server_name
+	mkdir $server_name
+	cd $server_name
+	nano start.sh
+	echo "Everything has been installed."
+fi
